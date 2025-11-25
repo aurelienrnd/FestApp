59fd87fd2981a5b0e98321ac5ca74088c2c59123
Aurélien <aurelienarnaud921@gmail.com>
2025-11-25 21:28:55 +0100

    Add commentaire on index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Définition d'une route GET sur /health pour tester si ton backend fonctionne correctement.
 app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "Backend is running" });
 });
 
+// Démarre l'écoute sur le port défini.
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
