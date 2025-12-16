b8942321a3d06bd9d3bcf82d91748a474506d9c0
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2025-12-16 11:02:47 +0100

    add file app.ts for the creation of express and Index only for the execution

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import { query } from "./db";
+
+// Création de l’application Express
+export function createApp() {
+  dotenv.config();
+
+  const app = express();
+
+  // test de demmarrage du serveur
+  app.get("/health", (req, res) => {
+    console.log("Health check successful serveur backend operationnel");
+    res.json({ status: "ok", message: "Backend is running" });
+  });
+
+  // test de connexion à la base de données
+  app.get("/debug/db", async (req, res) => {
+    try {
+      const rows = await query("SELECT NOW() as now");
+      console.log("Connexion to the DB successful at:", rows[0].now);
+      res.json({
+        db: "ok",
+        now: rows[0].now,
+      });
+    } catch (err) {
+      console.error("Erreur de connexion DB :", err);
+      res
+        .status(500)
+        .json({ error: "Impossible de joindre la base de donnees" });
+    }
+  });
+
+  return app;
+}
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,34 +1,14 @@
-import express from "express";
 import dotenv from "dotenv";
-import { query } from "./db";
+import { createApp } from "./app";
 
+// chargement des variables d’environnement
 dotenv.config();
 
-const app = express();
+// création de l’application Express
+const app = createApp();
 const PORT = process.env.PORT || 4000;
 
-// Définition d'une route GET sur /health pour tester si le backend fonctionne correctement.
-app.get("/health", (req, res) => {
-  console.log("Health check successful serveur backend opérationnel");
-  res.json({ status: "ok", message: "Backend is running" });
-});
-
-// Définition d'une route GET sur /debug/db pour tester la connexion à la base de données.
-app.get("/debug/db", async (req, res) => {
-  try {
-    const rows = await query("SELECT NOW() as now");
-    console.log("Connexion to the DB successful at:", rows[0].now);
-    res.json({
-      db: "ok",
-      now: rows[0].now,
-    });
-  } catch (err) {
-    console.error("Erreur de connexion DB :", err);
-    res.status(500).json({ error: "Impossible de joindre la base de données" });
-  }
-});
-
-// Démarre l'écoute sur le port défini.
+// lance le serveur sur le port spécifié
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
