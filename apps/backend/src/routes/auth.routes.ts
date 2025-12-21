4b0b9c1bc14549c4c291e615341c63ecf1a6ac64
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2025-12-21 21:20:20 +0100

    add auth.controllers.ts

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
+// midellewares
+import { validateBody } from "../middlewares/validateBody";
+import { hashPassword } from "../middlewares/hashPassword";
+// controllers
 import { testGetUsers, createUser } from "../controllers/auth.controller";
+//shema
+import { createUserSchema } from "../shemas/users.shema";
 
 const router = Router();
-router.post("/user", createUser); //NOTE cette routes est creer pour tester et cera deplacer plus tard
+router.post(
+  "/users",
+  validateBody(createUserSchema),
+  hashPassword(),
+  createUser,
+);
 router.get("/test-users", testGetUsers); //NOTE cette route est juste pour tester la connexion a la base de donnée, a supprimer plus tard
+
 export default router;
