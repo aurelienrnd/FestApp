bd6546772462b2fa1e97187974e3b43825e8fa08
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-02 19:35:00 +0100

    create function.ts file with function  getEnv, envToString, userExists, passwordIsValid, initToken, serializeCookie

---
+++ apps/backend/src/functions.ts
@@ -0,0 +1,54 @@
+import type { StringValue } from "ms";
+import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { serialize } from "cookie";
+import type { DbUser } from "./type..ts";
+
+
+export function getEnv(name: string): string {
+  const variables = process.env[name];
+  if (!variables) throw new Error(`Missing env var: ${name}`);
+  return variables;
+}
+
+export function envToString(name: string): StringValue | number {
+  const value = getEnv(name);
+  if (!/^\d+(\s?[a-zA-Z]+)?$/.test(value)) {
+    throw new Error(`Invalid duration for ${name}`);
+  }
+  return value as StringValue;
+}
+
+export function userExists(user: DbUser | undefined) {
+  if (!user) {
+    const message: string = "email ou mot de passe incorrect";
+    throw new Error("email ou mot de passe incorrect");
+  }
+}
+
+export function passwordIsValid(password: string, passwordHash: string) {
+  const isPasswordValid = bcrypt.compareSync(password, passwordHash);
+  if (!isPasswordValid) {
+    throw new Error("email ou mot de passe incorrect");
+  }
+}
+
+export function initToken(user: DbUser): string {
+  return jwt.sign({ sub: user.id, role: "admin" }, getEnv("JWT_SECRET"), {
+    expiresIn: envToString("JWT_EXPIRES_IN"), // jwt ne peut recevoir que des string ou number et non des Environment Variable
+  });
+}
+
+export function serializeCookie(token: string): string {
+  const cookieName = getEnv("COOKIE_NAME");
+  const secure = getEnv("COOKIE_SECURE") === "true"; // oblige le cookie à être transmis uniquement via HTTPS, cookie doit recevoir un boolean
+  const sameSite = getEnv("COOKIE_SAME_SITE") as "lax" | "strict" | "none"; // definit la politique SameSite pour le cookie
+
+  return serialize(cookieName, token, {
+    httpOnly: true, // empêche l'accès au cookie via JavaScript côté client, réduisant les risques de vol de cookie via des attaques XSS
+    secure,
+    sameSite,
+    path: "/", // rend le cookie accessible sur l'ensemble du site
+    maxAge: 60 * 60, // durée de vie du cookie en secondes (ici 1 heure)
+  });
+}
