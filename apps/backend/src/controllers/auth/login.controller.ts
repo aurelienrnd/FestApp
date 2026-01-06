9e3893f1b91a7b33473cb3cd8f5c4bde889169dc
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-06 20:47:25 +0100

    upadate: creation function generateSessions and generateAccessToken

--- apps/backend/src/controllers/auth/login.controller.ts
+++ apps/backend/src/controllers/auth/login.controller.ts
@@ -11,18 +11,63 @@ import {
 } from "../../functions";
 import type { DbUser } from "../../type.ts";
 
+async function generateSession(user: DbUser): Promise<string> {
+  //TODO expliquer
+  const refreshToken = crypto.randomBytes(64).toString("hex");
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  const expiresAt = new Date(
+    Date.now() + ms(envToStringValue("REFRESH_TOKEN_EXPIRES_IN")),
+  );
+
+  await query(
+    `INSERT INTO sessions (user_id, refresh_token_hash, expires_at) VALUES ($1, $2, $3)`,
+    [user.id, refreshTokenHash, expiresAt],
+  );
+
+  const refreshCookie = serializeCookie(
+    "COOKIE_REFRESH_TOKEN_NAME",
+    "COOKIE_REFRESH_TOKEN_SECURE",
+    "COOKIE_REFRESH_TOKEN_SAME_SITE",
+    refreshToken,
+    "REFRESH_TOKEN_EXPIRES_IN",
+  );
+
+  return refreshCookie;
+}
+
+function generateAccessToken(user: DbUser): string {
+  // générer un jwt pour le accessToken
+  const accessToken = initToken(
+    user,
+    "JWT_ACCESS_SECRET",
+    "JWT_ACCESS_EXPIRES_IN",
+  );
+
+  const accessCookie = serializeCookie(
+    "COOKIE_ACCESS_TOKEN_NAME",
+    "COOKIE_ACCESS_TOKEN_SECURE",
+    "COOKIE_ACCESS_TOKEN_SAME_SITE",
+    accessToken,
+    "JWT_ACCESS_EXPIRES_IN",
+  );
+
+  return accessCookie;
+}
+
 /**TODO A terminer
- - implementer logique pour "session.user_agent" "session.ip"
  - implementer logique role dans le access token
- - Decouper en fonction 
  - Commenter
 */
 export async function login(req: Request, res: Response) {
-  // on verifie que l'email et le password sont fournis dans le bon format
-  const email = String(req.body.email).trim().toLowerCase();
-  const password = String(req.body.password);
-
   try {
+    // on verifie que l'email et le password sont fournis dans le bon format
+    const email = String(req.body.email).trim().toLowerCase();
+    const password = String(req.body.password);
+
     // Creation de la requette SQL pour recuperer l'utilisateur par email
     const result = await query<DbUser>(
       `SELECT id, email, password_hash, display_name, is_active
@@ -33,54 +78,16 @@ export async function login(req: Request, res: Response) {
     );
     const user = result[0];
 
-    // on verifie que l'utilisateur existe
+    // on verifie que l'utilisateur existe et que le mot de passe est correct
     userExists(user);
-
-    // on verifie que le mot de passe est correct
     passwordIsValid(password, user.password_hash);
 
-    //TODO expliquer
-    const refreshToken = crypto.randomBytes(64).toString("hex");
-    const refreshTokenHash = crypto
-      .createHash("sha256")
-      .update(refreshToken)
-      .digest("hex");
-
-    const expiresAt = new Date(
-      Date.now() + ms(envToStringValue("REFRESH_TOKEN_EXPIRES_IN")),
-    );
-
-    await query(
-      `INSERT INTO sessions (user_id, refresh_token_hash, expires_at) VALUES ($1, $2, $3)`,
-      [user.id, refreshTokenHash, expiresAt],
-    );
-
-    // générer un jwt pour le accessToken
-    const accessToken = initToken(
-      user,
-      "JWT_ACCESS_SECRET",
-      "JWT_ACCESS_EXPIRES_IN",
-    );
-
-    const accessCookie = serializeCookie(
-      "COOKIE_ACCESS_TOKEN_NAME",
-      "COOKIE_ACCESS_TOKEN_SECURE",
-      "COOKIE_ACCESS_TOKEN_SAME_SITE",
-      accessToken,
-      "JWT_ACCESS_EXPIRES_IN",
-    );
-
-    const refreshCookie = serializeCookie(
-      "COOKIE_REFRESH_TOKEN_NAME",
-      "COOKIE_REFRESH_TOKEN_SECURE",
-      "COOKIE_REFRESH_TOKEN_SAME_SITE",
-      refreshToken,
-      "REFRESH_TOKEN_EXPIRES_IN",
-    );
-
-    // ajout du cookie dans le header de la reponse
+    //Creation des cookies et ajout du cookie dans le header de la reponse
+    const refreshCookie: string = await generateSession(user);
+    const accessCookie: string = generateAccessToken(user);
     res.setHeader("Set-Cookie", [accessCookie, refreshCookie]);
 
+    // Edit la reponsse
     const userSession = await query(
       `SELECT id, user_id, expires_at, created_at FROM sessions WHERE $1 = user_id ORDER BY created_at DESC LIMIT 1`,
       [user.id],
