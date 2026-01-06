51b799e88adc67a86bbfd6fcc6a7e179d79bf4d9
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-06 20:21:24 +0100

    update reponsse userSession

--- apps/backend/src/controllers/auth/login.controller.ts
+++ apps/backend/src/controllers/auth/login.controller.ts
@@ -11,6 +11,12 @@ import {
 } from "../../functions";
 import type { DbUser } from "../../type.ts";
 
+/**TODO A terminer
+ - implementer logique pour "session.user_agent" "session.ip"
+ - implementer logique role dans le access token
+ - Decouper en fonction 
+ - Commenter
+*/
 export async function login(req: Request, res: Response) {
   // on verifie que l'email et le password sont fournis dans le bon format
   const email = String(req.body.email).trim().toLowerCase();
@@ -33,7 +39,7 @@ export async function login(req: Request, res: Response) {
     // on verifie que le mot de passe est correct
     passwordIsValid(password, user.password_hash);
 
-    //NOTE expliquer
+    //TODO expliquer
     const refreshToken = crypto.randomBytes(64).toString("hex");
     const refreshTokenHash = crypto
       .createHash("sha256")
@@ -44,9 +50,6 @@ export async function login(req: Request, res: Response) {
       Date.now() + ms(envToStringValue("REFRESH_TOKEN_EXPIRES_IN")),
     );
 
-    console.log(expiresAt);
-
-    //NOTE modiffier Date.now() + "REFRESH_TOKEN_EXPIRES_IN in dateTime Type
     await query(
       `INSERT INTO sessions (user_id, refresh_token_hash, expires_at) VALUES ($1, $2, $3)`,
       [user.id, refreshTokenHash, expiresAt],
@@ -78,15 +81,15 @@ export async function login(req: Request, res: Response) {
     // ajout du cookie dans le header de la reponse
     res.setHeader("Set-Cookie", [accessCookie, refreshCookie]);
 
-    const repSessions = await query(
-      `SELECT * FROM sessions WHERE $1 = user_id ORDER BY created_at DESC LIMIT 1`,
+    const userSession = await query(
+      `SELECT id, user_id, expires_at, created_at FROM sessions WHERE $1 = user_id ORDER BY created_at DESC LIMIT 1`,
       [user.id],
     );
 
     // reponse avec les informations de l'utilisateur
     return res.status(200).json({
       message: "Authentification réussie",
-      repSessions,
+      userSession,
     });
   } catch (error: any) {
     console.error(error);
