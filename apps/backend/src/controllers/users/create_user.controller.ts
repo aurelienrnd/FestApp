981c0e59dfb6b628477f6d34a415aa699fdb0a89
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-14 12:18:58 +0100

    update create_user_controller : remove of updated_at from the requet to the bdd

--- apps/backend/src/controllers/users/create_user.controller.ts
+++ apps/backend/src/controllers/users/create_user.controller.ts
@@ -44,7 +44,7 @@ export const createUser = async (req: Request, res: Response) => {
     const row = await query(
       `INSERT INTO users (email, password_hash, display_name, must_change_password, is_active)
        VALUES ($1, $2, $3, TRUE, TRUE)
-       RETURNING id, email, display_name, is_active, must_change_password, created_at, updated_at`,
+       RETURNING id, email, display_name, is_active, must_change_password, created_at`,
       [email, password, display_name],
     );
 
