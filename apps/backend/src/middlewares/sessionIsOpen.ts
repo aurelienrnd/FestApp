da0f0d04df7dabf4dd34637fb5711086219cb9e7
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-15 14:48:46 +0100

    update seesionIOpen.ts : add a try catch

--- apps/backend/src/middlewares/sessionIsOpen.ts
+++ apps/backend/src/middlewares/sessionIsOpen.ts
@@ -18,44 +18,50 @@ export async function sessionIsOpen(
   res: Response,
   next: NextFunction,
 ) {
-  // récupération des info du headers
-  const reqSessionId = req.headers.session;
-  const reqUserId = req.headers.userId;
+  try {
+    // récupération des info du headers
+    const reqSessionId = req.headers.session;
+    const reqUserId = req.headers.userId;
 
-  // requête en BDD recupérant la session
-  const row = await query(
-    "SELECT id, user_id, expires_at, revoked_at FROM sessions WHERE id = $1 AND user_id = $2",
-    [reqSessionId, reqUserId],
-  );
-  const sessionBdd = row[0];
+    // requête en BDD recupérant la session
+    const row = await query(
+      "SELECT id, user_id, expires_at, revoked_at FROM sessions WHERE id = $1 AND user_id = $2",
+      [reqSessionId, reqUserId],
+    );
+    const sessionBdd = row[0];
 
-  // Verifie si la session est valide
-  if (!sessionBdd) {
-    return res.status(401).json({ message: "Session invalide" });
-  } else if (
-    sessionBdd.revoked_at !== null ||
-    new Date(sessionBdd.expires_at) < new Date()
-  ) {
-    return res.status(401).json({ message: "Session révoquée ou expirée" });
+    // Verifie si la session est valide
+    if (!sessionBdd) {
+      return next(new Error("session not found"));
+    } else if (
+      sessionBdd.revoked_at !== null ||
+      new Date(sessionBdd.expires_at) < new Date()
+    ) {
+      return next(new Error("session  already closed"));
+    }
+
+    // Si la session est valide, on renouvelle le token et le cookie
+    const accessToken = initToken(
+      reqUserId as string,
+      "JWT_ACCESS_SECRET",
+      "JWT_ACCESS_EXPIRES_IN",
+      reqSessionId as string,
+    );
+    //TODO - remove it
+    console.log("nouveau token", accessToken);
+    const accessCookie = serializeCookie(
+      "COOKIE_ACCESS_TOKEN_NAME",
+      "COOKIE_ACCESS_TOKEN_SECURE",
+      "COOKIE_ACCESS_TOKEN_SAME_SITE",
+      accessToken,
+      "JWT_ACCESS_EXPIRES_IN",
+    );
+    res.setHeader("Set-Cookie", accessCookie);
+
+    next();
+  } catch (error: any) {
+    return res.status(401).json({
+      error: error.message || "Unauthorized access",
+    });
   }
-
-  // Si la session est valide, on renouvelle le token et le cookie
-  const accessToken = initToken(
-    reqUserId as string,
-    "JWT_ACCESS_SECRET",
-    "JWT_ACCESS_EXPIRES_IN",
-    reqSessionId as string,
-  );
-  //TODO - remove it
-  console.log("nouveau token", accessToken);
-  const accessCookie = serializeCookie(
-    "COOKIE_ACCESS_TOKEN_NAME",
-    "COOKIE_ACCESS_TOKEN_SECURE",
-    "COOKIE_ACCESS_TOKEN_SAME_SITE",
-    accessToken,
-    "JWT_ACCESS_EXPIRES_IN",
-  );
-  res.setHeader("Set-Cookie", accessCookie);
-
-  next();
 }
