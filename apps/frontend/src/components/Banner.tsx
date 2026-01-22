de6b8998aefaf5c66b748628d66cb4f4a7f3f725
aurelienrnd <145446918+aurelienrnd@users.noreply.github.com>
2026-01-22 21:15:46 +0100

    - initialisation Home composent -  creation Banner and Footer composents - add koulen font in layourt.tsx a,d add metadata

---
+++ apps/frontend/src/components/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <header className="border border-white">
+      <p>Vind Hellfest</p>
+    </header>
+  );
+}
---
+++ apps/frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="border border-white">
+      <p>Vind Hellfest</p>
+    </footer>
+  );
+}
