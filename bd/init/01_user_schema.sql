ac981fcda2f5f748105ab7c0e3f2610d0f4b8a15
Aurélien <aurelienarnaud921@gmail.com>
2026-03-12 23:26:48 +0100

    bd/init/01_user_schema.sql Remove is_active from users schema and seed data.

--- bd/init/01_user_schema.sql
+++ bd/init/01_user_schema.sql
@@ -1,4 +1,4 @@
--- Extensions nécessaires
+﻿-- Extensions nécessaires
 CREATE EXTENSION IF NOT EXISTS pgcrypto;   -- Pour les UUID aléatoires (gen_random_uuid)
 CREATE EXTENSION IF NOT EXISTS citext;     -- Pour rendre les emails insensibles à la casse
 
@@ -12,20 +12,19 @@ CREATE TABLE users (
   email CITEXT NOT NULL UNIQUE,                         -- Email insensible à la casse, doit être unique
   password_hash VARCHAR(255) NOT NULL,                  -- Mot de passe chiffré (hashé en backend)
   display_name VARCHAR(100) NOT NULL,                   -- Nom affiché dans l’espace d’administration
-  is_active BOOLEAN NOT NULL DEFAULT TRUE,              -- Permet de désactiver un compte sans le supprimer
-  must_change_password BOOLEAN NOT NULL DEFAULT FALSE,  -- Mot de passe provisoire : changement obligatoire au prochain login
+  must_change_password BOOLEAN NOT NULL DEFAULT TRUE,  -- Mot de passe provisoire : changement obligatoire au prochain login
   role VARCHAR(50) NOT NULL DEFAULT 'user',             -- Role de l'utilisateur
   password_changed_at TIMESTAMPTZ NULL,                 -- Date/heure du dernier changement de mot de passe
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()         -- Date de création
 );
 
 -- //NOTE : Utiliser uniquement en phase de développement.
-INSERT INTO users (email, password_hash, display_name, is_active, must_change_password, role)
+INSERT INTO users (email, password_hash, display_name, must_change_password, role)
 VALUES (
   'admin@example.com',
   '$2b$10$GwPZLi7S9h9O/Y9zljjQzesw2SIUr3APiro6Yz3/HamsQN.TyPj.C', -- Hash bcrypt du mot de passe 'MyPassword'
   'Admin',
-  TRUE,
   FALSE,
   'admin'
 );
+
