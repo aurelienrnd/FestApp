efb1862ddeaa73f0ad776d34def87664b39c442c
Aurélien <aurelienarnaud921@gmail.com>
2026-04-11 10:05:41 +0200

    style(frontend): add date info blocks to home hero and resize CTA button

--- apps/frontend/src/app/(public)/HomeHero.tsx
+++ apps/frontend/src/app/(public)/HomeHero.tsx
@@ -10,21 +10,44 @@ export default function HomeHero() {
       className="home-hero"
       style={{ backgroundImage: "url('/hero_bg.webp')" }}
     >
-      <div className="w-full h-2/3 border border-white flex flex-col items-center justify-start mt-(--header-height)">
+      <div className="w-full h-2/3 flex flex-col items-center justify-start mt-(--header-height)">
         <Image
           src={logo}
           alt="Logo Vindhellfest"
           priority
-          className="home-hero-logo py-(--margin-bottom-title)"
+          className="py-(--margin-bottom-title)"
         />
-        <a
-          href="https://www.google.com/search?q=tiket+master&rlz=1C1ONGR_frFR1184FR1184&oq=tiket+master&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDM1ODJqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8"
-          className="btn-cta home-hero-btn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Billetterie
-        </a>
+
+        <div className="w-full h-full flex flex-row-reverse items-center justify-between">
+          <div className="h-full flex justify-around flex-col gap-4">
+            <a
+              href="https://www.google.com/search?q=tiket+master&rlz=1C1ONGR_frFR1184FR1184&oq=tiket+master&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDM1ODJqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8"
+              className="btn-cta home-hero-btn mr-(--space-lg)"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Billetterie
+            </a>
+            <span className="bg-white w-40 text-black text-lg flex justify-center items-center py-(--space-md)">
+              VINDELLE
+            </span>
+          </div>
+
+          <div className="h-full flex justify-around flex-col gap-4">
+            <span className="bg-white w-30 text-black text-lg flex justify-center items-center py-(--space-md)">
+              DU
+            </span>
+            <span className="bg-white w-40 text-black text-lg flex justify-center items-center py-(--space-md)">
+              SAMEDI 23 AOUT
+            </span>
+            <span className="bg-white w-30 text-black text-lg flex justify-center items-center py-(--space-md)">
+              AU
+            </span>
+            <span className="bg-white w-40 text-black text-lg flex justify-center items-center py-(--space-md)">
+              DIMANCHE 24 AOUT
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
