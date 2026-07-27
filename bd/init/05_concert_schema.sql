671d60efb801eee7a6cd77e82d15cf0f491f6a21
Aurélien <aurelienarnaud921@gmail.com>
2026-07-27 21:09:09 +0200

    chore(db): exposer PostgreSQL sur le port hôte 5433 et retirer l'extension citext inutilisée
    - docker-compose: mappe le port 5433:5432 sur le service db pour permettre la connexion via pgAdmin sans conflit avec un PostgreSQL local
    - bd/init: supprime CREATE EXTENSION citext (non utilisée par le schéma concert)

--- bd/init/05_concert_schema.sql
+++ bd/init/05_concert_schema.sql
@@ -1,6 +1,5 @@
 -- Extensions necessaires
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS citext;
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 -- //NOTE : Utiliser uniquement en phase de developpement.
