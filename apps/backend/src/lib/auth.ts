b5a364cc4b2229a85374a4e9357b9b6fbaeedb79
Aurélien <aurelienarnaud921@gmail.com>
2026-08-29 11:28:31 +0200

    add connexion a la bd via import pool

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth";
+import { pool } from "../db.js";
 
 export const auth = betterAuth({
-  //...
+  // connexion BDD
+  database: pool,
+
+  // origine autorisée à envoyer des requêtes avec credentials (cookies)
+  trustedOrigins: [process.env.FRONTEND_ORIGIN ?? "http://localhost:3000"],
 });
